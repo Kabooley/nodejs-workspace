749063ea11a8f615a19bf8e6bb28ca1ef37f1c2a
Kabooley <yashiki.doh.ta1he1@gmail.com>
2022-07-29 21:26:03 +0900

    nothing specific

--- workspace/src/index.ts
+++ workspace/src/index.ts
@@ -1,1 +1,30 @@
-console.log('Hello, TypeScript!');
+// const https = require("https");
+const path = require("path");
+
+import https from "https";
+
+const options = {
+    hostname: 'raw.githubusercontent.com',
+    method: 'GET',
+    path: '/wiki/Microsoft/DirectXTK/images/cat.png'
+};
+
+let body = '';
+
+// req: ClientRequest
+const req = https.request(options, (res) => {
+    console.log(`Status Code:${res.statusCode}`);
+    console.log(`Headers: ${res.headers}`);
+
+    res.on('data', (d) => {
+        // write to somewhere
+    })
+})
+
+req.on('error', (e) => {
+    console.error(e);
+})
+req.write(chunk, (e) => {
+
+});
+req.end();
