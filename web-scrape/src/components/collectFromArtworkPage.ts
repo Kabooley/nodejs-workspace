77c7f4b4f9ae053fe7df64369394fa95ab2be096
Kabooley <yashiki.doh.ta1he1@gmail.com>
2022-09-29 21:58:54 +0900

    Feat: Implementing collctor of artwork page and too many fix requirement

--- web-scrape/src/components/collectFromArtworkPage.ts
+++ web-scrape/src/components/collectFromArtworkPage.ts
@@ -7,11 +7,14 @@
  * */
 
 import type puppeteer from "puppeteer";
-import { Collect } from './Collect';
 import { Navigation } from './Navigation';
 import { selectors } from '../constants/selectors';
 
-let artworkUrl: string = `https://www.pixiv.net/artworks/`;
+interface iRequirement {
+    /* NOTE: key of iArtworkData にすべきか、決め打ちしちゃうか... */ 
+};
+
+interface iCollectionFromArtwork {};
 
 interface iBodyOfArtworkPageResponse {
     error: boolean;
@@ -33,11 +36,11 @@ interface iArtworkData {
     xRestrict?:number;
     sl:string;
     urls:{
-        mini?:string;
-        thumb?:string;
-        small?:string;
-        regular?:string;
-        original:string;    // 取得したい情報
+        mini?:string;       // 実際はstringではなくて正規表現である
+        thumb?:string;      // 実際はstringではなくて正規表現である
+        small?:string;      // 実際はstringではなくて正規表現である
+        regular?:string;    // 実際はstringではなくて正規表現である
+        original:string;    // 実際はstringではなくて正規表現である。取得したい情報。
     },
     tags?: any;
     pageCount: number;		// 多分一枚目以外の画像枚数
@@ -52,28 +55,103 @@ interface iArtworkData {
  * 
  * @return {iArtworkData[]} - 
  * */ 
-const collectFromArtworkPage = async (
-    page: puppeteer.Page, 
-    ids: (keyof iArtworkData)[],
-    requirement: iArtworkData[]
-    ): Promise<void> => {
-    try {
-        // page.goto(artworkUrl + id);
-        // navigation
-        // get http response (to check navigation is ok)
-        // check response
-        // collect information from response
-        // next
 
-        let navigation = new Navigation(page);
-        let collector = new Collect();
+ const artworkUrl: string = `https://www.pixiv.net/artworks/`;
 
+//  Only returns callback function only for page.waitForResponse()
+ const getCallback = function(requiredUrl: string): ((res: puppeteer.HTTPResponse) => boolean) {
+    const cb = (res: puppeteer.HTTPResponse): boolean => {
+        return res.status() === 200 && res.url().includes(requiredUrl);
+    };
+    return cb;
+ };
 
-        let currentPage: number = 1;
-        let lastPage: number = 0;
-    }
-    catch(e) {
-        throw e;
-    }
-}
+/***
+ * Navigation.navigateBy()で返されるHTTPResponseなどからなる配列から、
+ * page.waitForResponse()から返された戻り値だけを抜き出して返す。
+ * 
+ * NOTE: 引数であるresがNavigation.navigateBy()の戻り値で、
+ * 且つそれの最初の要素がwaitForResponse()の戻り値であることを大前提にしている。
+ * 
+ * TODO: 次の実装
+ * res : (puppeteer.HTTPResponse | any)[] からres[0]を抜き出して、
+ * res[0]がiBodyOfArtworkPageResponse型であることのチェックして合格したら
+ * res[0].bodyがiArtworkDataであることのチェック
+ * これらに合格したらres[0].bodyを返す
+ * 
+ * res[0].object.hasOwnProperty(keyof iBodyOfArtworkPageResponse)は可能？
+ * res[0].bodyが存在するのかのチェックの方がいいかな
+ * それだとハードコーディングなんだけど、
+ * それをいうとこの関数をすごく汎用的にしないといけないなぁ
+ * 
+ * ならば機能を分割した方がいいねぇ
+ * */  
+ const removeFromResponse = async <T>(res: (puppeteer.HTTPResponse | any)[]): Promise<T> => {
+    if(!res[0]) throw new Error("Something went wrong but required HTTP Response could not captured.");
+    return res[0];
+ };
+ 
+ const isFulfillRequirement = (body: iArtworkData, requirement): boolean => {
+     // とにかくrequirementを満たすかどうかチェックする
+     // 満たすならtrue、そうじゃないならfalse
+     return result;
+ };
+ 
 
+/****
+ * @param {} - 
+ * @param {} - 
+ * @param {iRequirement} [requirement] -  
+ * @param {} - 
+ * 
+ * */  
+ const collectArtworksData = async (
+        page: puppeteer.Page, 
+        passedIds: string[], 
+        requirement?: iRequirement
+    ): Promise<iArtworkData[]> => {
+ 
+     // Set up navigation.
+     const navigate = new Navigation(page);
+ 
+     let res: (puppeteer.HTTPResponse | any)[] = [];
+     let collected: iArtworkData[] = [];
+     let promise: Promise<void> = Promise.resolve();
+     let cb: (r: puppeteer.HTTPResponse) => boolean;
+
+     for(const id of passedIds) {
+         // -- ここの囲った部分は終わるまで ---------------------
+         // 次のナビゲーション(ループ)に行くことは許されない
+         
+        //  毎ループ、responseフィルタリングのためにwaitForResponseのコールバックを更新しなくてはならない
+         cb = getCallback(artworkUrl + id)
+         navigate.resetWaitForResponseCallback(page.waitForResponse(cb));
+        //  TODO: Navigateクラスの修正
+         res = await navigate.navigateBy(page.goto(artworkUrl + id));
+         // bodyとはHTTPResponse.body.bodyである
+         const body: iArtworkData = await removeFromResponse<iArtworkData>(res);
+         // --------------------------------
+ 
+         // -- ここの条件分岐は非同期にして次のページ遷移に行っちゃっていい --
+         // なのでプロミスで囲ってあとで終わればOKにすればいいかも。
+         // 
+         // !requirement.length --> bodyをそのまま納める
+         // requirement.length --> 次を検査する
+         // isFullfillRequirement() --> bodyを納める
+         // !isFullfillRequirement() --> bodyは納めない
+         promise = promise.then(() => {
+             if(requirement.length) {
+                 if(!isFulfillRequirement(body)) collected.push(body);
+             }
+             else {
+                 collected.push(body);
+             }
+         });
+         // ---------------------------------------------------
+     };
+ 
+     await promise;
+     return collected;
+ };
+ 
+ 
