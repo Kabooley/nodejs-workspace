568418d626a2a74dcca4f53ba78ed06620ba47d6
Kabooley <35451559+Kabooley@users.noreply.github.com>
2022-10-10 21:51:53 +0900

    Added parallelCollectArtworkdata.ts

---
+++ web-scrape/temporary/parallelCollectArtworkdata.ts
@@ -0,0 +1,153 @@
+/***************************************************************
+ * 逐次処理：
+ * 
+ * 下記の一連の処理が完了して初めて次のページへ遷移できる
+ * 
+ * - navigation by page.goto()
+ * - retrieve data form httpresponse body
+ * - （ここは後回しでもいい）push data to collection array
+ * 
+ * 理由は、
+ * HTTPレスポンスからデータ取得するときに使うpuppeteer.HTTPResponse.text()はそのページにいるときじゃないと
+ * 取得できないから。
+ * 
+ * なので、同一のpageインスタンスに対して並列処理はできない。
+ * 
+ * ということで、pageインスタンスを増やして、pageインスタンス同士を並列処理させる。
+ * 
+ * TODO: ループで関数をスマートに順番に呼び出したい
+ * 参考：https://stackoverflow.com/a/52985373
+ * *************************************************************/ 
+import type puppeteer from 'puppeteer';
+import { Navigation } from './components/Navigation';
+
+// up to two parallel process
+const navigation = new Navigation();
+
+const executor = async (p: puppeteer.Page, id: string, container: iDataWhatIwant[]): Promise<void> => {
+    try {
+        const navigateResults = await navigation.navigateBy(p.goto(`url/${id}`, { waitUntil: ["load", "networkidle2"]}));
+        container = [...container, ...(await captureBodyData(navigationResults))];
+    }
+    catch(e) {
+        throw e;
+    }
+};
+
+// TODO: pageインスタンスは一つだけでいいなら条件分岐で別の処理にするべきか検討
+const collectArtworkData = async (browser: puppeteer.Browser, page: puppeteer.Page, ids: string[]): Promise<iDataWhatIwant[]> => {
+    try {
+        let collected: iDataWhatIwant[] = [];
+        let sequences: Promise<void>[] = [];
+        let pageInstances: puppeteer.Page[] = [];
+        let concurrency: number = 1;
+
+        // データ量が50以下なら処理プロセス２で並列処理
+        if(ids.length > 10 && ids.length <= 50) {
+            concurrency = 2;
+        }
+        // データ量５０を超えるなら処理プロセス３で並列処理
+        else if(ids.length > 50) {
+            concurrency = 4;
+        }
+
+        pageInstances.push(page);
+        for(let i = 1; i < concurrency; i++) {
+            pageInstances.push(await browser.newPage());
+            sequences.push(Promise.resolve());
+        };
+        ids.forEach((id: string, index: number) => {
+            // 
+            // TODO: 限定された範囲の数を順番にループする方法はindex % concurrencyで正しいか確認
+            // 
+            sequences[index % concurrency] = sequences[index % concurrency].then(() => executor(pageInstances[index % concurrency], id));
+        });
+
+        await Promise.all([...sequences]);
+        return collected;
+
+    }
+    catch(e) {
+        await page.screenshot({type: 'png', path: './dist/errorWhileCollectingArtworkData.png'});
+        throw e;
+    }
+    finally {
+        /* 新たに生成したpageインスタンスをどうにかcloseさせたい */ 
+        while(pageInstances.length) {
+            
+        }
+    }
+}
+
+
+// const collectArtworkData = async (browser: puppeteer.Browser, page: puppeteer.Page, ids: string[]): Promise<iDataWhatIwant[]> => {
+//     try {
+//         let collected: iDataWhatIwant[] = [];
+//         let tasks: (() => Promise<void>)[] = [];
+//         let pageInstances: puppeteer.Page[] = [];
+
+//         // データ量が50以下なら処理プロセス２で並列処理
+//         if(ids.length > 10 && ids.length <= 50) {
+//             const temporaryPage1: puppeteer.Page = browser.newPage();
+
+//             let it: string[] = [...ids];
+//             let sequence1 = Promise.resolve();
+//             let sequence2 = Promise.resolve();
+            
+//             while(it.length > 0) {
+//                 sequence1 = sequence1.then(() => executor(page, it!.pop());
+//                 if(it.length >= 0) sequence2 = sequence2.then(() => executor(temporaryPage1, it!.pop()));
+//             } 
+            
+//             await Promise.all([sequence1, sequence2]);
+//             temporaryPage1.close();
+//             temporaryPage1 = undefined;
+//         }
+//         // データ量５０を超えるなら処理プロセス３で並列処理
+//         else if(ids.length > 50) {
+//             const temporaryPage1: puppeteer.Page = browser.newPage();
+//             const temporaryPage2: puppeteer.Page = browser.newPage();
+
+//             let it: string[] = [...ids];
+//             let sequence1 = Promise.resolve();
+//             let sequence2 = Promise.resolve();
+//             let sequence3 = Promise.resolve();
+            
+//             while(it.length > 0) {
+//                 sequence1 = sequence1.then(() => executor(page, it!.pop());
+//                 if(it.length >= 0) sequence2 = sequence2.then(() => executor(temporaryPage1, it!.pop()));
+//                 if(it.length >= 0) sequence3 = sequence3.then(() => executor(temporaryPage2, it!.pop()));
+//             } 
+            
+//             await Promise.all([sequence1, sequence2, sequence3]);
+//             temporaryPage1.close();
+//             temporaryPage2.close();
+//             temporaryPage1 = undefined;
+//             temporaryPage2 = undefined;
+//         }
+//         // データ量少ないなら逐次処理だけ
+//         else {
+//             let sequence = Promise.resolve();
+//             ids.forEach((id: string) => {
+//                 sequence = sequence.then(() => {
+//                     executor(page, id)
+//                 });
+//             });
+
+//             sequence.then(() => {
+
+//             }).catch(e => {throw e});
+//         }
+
+//         return collected;
+
+//     }
+//     catch(e) {
+//         await page.screenshot({type: 'png', path: './dist/errorWhileCollectingArtworkData.png'});
+//         throw e;
+//     }
+//     finally {
+//         /* 新たに生成したpageインスタンスをどうにかcloseさせたい */ 
+//     }
+// }
+
