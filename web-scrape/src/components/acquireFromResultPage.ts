a725a44aa21fff9d3a67d232dde5acb8da712b18
Kabooley <yashiki.doh.ta1he1@gmail.com>
2022-11-14 16:43:20 +0900

    Converting function to class

--- web-scrape/src/components/acquireFromResultPage.ts
+++ web-scrape/src/components/acquireFromResultPage.ts
@@ -17,15 +17,114 @@
  * 
  * *****************************************************/ 
 import type puppeteer from 'puppeteer';
+import type { iIllustMangaDataElement, iBodyIncludesIllustManga } from '../constants/illustManga';
 import { Collect } from './Collect';
 import { Navigation} from './Navigation';
-import type { iIllustMangaDataElement, iIllustManga, iBodyIncludesIllustManga } from '../constants/illustManga';
 import { retrieveDeepProp } from '../utilities/objectModifier';
+
+export class CollectResultPage<T> {
+    pageInstances: puppeteer.Page[] = [];
+    sequences: Promise<void>[] = [];
+    concurrency: number;
+    navigation: Navigation;
+    collected: T[];
+    constructor(private browser: puppeteer.Browser, numberOfProcess: number, numberOfPages: number, keyword: string){
+        this.concurrency = numberOfProcess;
+        this.navigation = new Navigation();
+        this.collected = [];
+    };
+
+    async _generatePageInstances() {
+        return this.pageInstances.push(await this.browser.newPage());
+    };
+
+    _initializeSequences() {
+        return this.sequences.push(Promise.resolve());
+    };
+
+    _resetNavigationFilter(url: string) {
+        this.navigation.resetFilter((response: puppeteer.HTTPResponse) => 
+            response.status() === 200 && response.url() === url
+        );
+    };
+
+    initialize() {
+        for(let i = 0; i < this.numberOfProcess; i++) {
+            this._initializeSequences();
+            // TODO: async関数をいかにして呼出すか...
+        }
+    };
+
+    _executor(page: puppeteer.Page, navigation: Navigation, currentPage: number): Promise<void> {
+        try {
+            const url: string = `https://www.pixiv.net/ajax/search/artworks/${encodeURIComponent(keyword)}?word=${encodeURIComponent(keyword)}&order=date_d&mode=all&p=${currentPage}&s_mode=s_tag&type=all&lang=ja`;
+    
+            // setup navigation filter.
+            navigation.resetFilter((res: puppeteer.HTTPResponse) => res.status() === 200 && res.url() === url);
+            // let it navigate to the url.
+            const responces: (puppeteer.HTTPResponse | any)[] = await navigation.navigateBy(page, page.goto(url));
+            // HTTPResponseから必要なデータを取得する
+            const illustMangaDataElements: iIllustMangaDataElement[] = retrieveDeepProp<iIllustMangaDataElement[]>(["body", "illustManga", "data"], (await responces.shift().json()) as iBodyIncludesIllustManga); 
+            // Check if the data is exist
+            if(illustMangaDataElements === undefined) throw new Error("Error: ")
+            // Retrieve and save. 
+            collector.resetData(illustMangaDataElements);
+            collectedIds = [...collectedIds, ...collector.execute("id")];
+        }
+        catch(e) {
+            console.error(e);
+            throw e;
+        }
+
+    }
+
+    generateTasks() {
+        for(let currentPage = 1; currentPage < numberOfPages; currentPage++) {
+            const circulator: number = currentPage % concurrency;
+            if(this.sequences[circulator] !== undefined) {
+                this.sequences[circulator] = this.sequences[circulator]!
+                // Reset navigation filter function.
+                .then(() => this._executor(this.pageInstances[circulator]!, this.navigation, currentPage))
+            }
+            else {
+                console.error("RangeError: Accessing out range of array.");
+            }
+        }
+        return this.sequences;
+    }
+
+    // finally() will not be invoked automatically.
+    // But this method must be called after all process is done.
+    finally() {
+
+    }
+}
 
 // 検索結果の各artworkのidを格納する
 let collectedIds: string[] = [];
 const collector: Collect<iIllustMangaDataElement> = new Collect<iIllustMangaDataElement>();
-const filterUrl: string = "";
+
+const executor = async (page: puppeteer.Page, navigation: Navigation, keyword: string, currentPage: number) => {
+    try {
+        const url: string = `https://www.pixiv.net/ajax/search/artworks/${encodeURIComponent(keyword)}?word=${encodeURIComponent(keyword)}&order=date_d&mode=all&p=${currentPage}&s_mode=s_tag&type=all&lang=ja`;
+
+        // setup navigation filter.
+        navigation.resetFilter((res: puppeteer.HTTPResponse) => res.status() === 200 && res.url() === url);
+        // let it navigate to the url.
+        const responces: (puppeteer.HTTPResponse | any)[] = await navigation.navigateBy(page, page.goto(url));
+        // HTTPResponseから必要なデータを取得する
+        const illustMangaDataElements: iIllustMangaDataElement[] = retrieveDeepProp<iIllustMangaDataElement[]>(["body", "illustManga", "data"], (await responces.shift().json()) as iBodyIncludesIllustManga); 
+        // Check if the data is exist
+        if(illustMangaDataElements === undefined) throw new Error("Error: ")
+        // Retrieve and save. 
+        collector.resetData(illustMangaDataElements);
+        collectedIds = [...collectedIds, ...collector.execute("id")];
+    }
+    catch(e) {
+        console.error(e);
+        throw e;
+    }
+}
 
 
 
@@ -36,14 +135,22 @@ const filterUrl: string = "";
  * というようにしたい
  * 
  * TODO: finallyを組み込んでここで生成したpageインスタンスを必ずcloseする
+ *  classにした方がいいのかも
  * 
+ * TODO: async/awaitとPromiseチェーンが両方存在している問題
  * 
+ * promise = promise.then(() => somethinfIwannaDo())をループさせて行くので
+ * somethinfIWannaDoのなかでpromiseチェーンを形成するわけにはいかない
  * */ 
-const setupTasks = (
+// VER.2
+// then() returns async function.
+// TODO: Make sure returning async function from then() is correct.
+export const setupTasks = async (
     browser: puppeteer.Browser,
     numberOfProcess: number,    // 作成するpageインスタンスの数
     numberOfPages: number,       // 検索結果ページがなんページなのか
-    ): Promise<void>[] => {
+    keyword: string             // 検索キーワード
+    ): Promise<Promise<void>[]> => {
     let concurrency: number = numberOfProcess;
     const pageInstances: puppeteer.Page[] = [];
     const sequences: Promise<void>[] = [];
@@ -58,33 +165,64 @@ const setupTasks = (
         const circulator: number = currentPage % concurrency;
         if(sequences[circulator] !== undefined) {
             sequences[circulator] = sequences[circulator]!
-            .then(() => {
-                navigation.resetFilter((res: puppeteer.HTTPResponse) => 
-                    // TODO: filterUrl = "https:....?p=${i}"となるURLにすること
-                    res.status() === 200 && res.url() === filterUrl
-                );
-                
-                // TODO: awaitのエラーはthne()のハンドラ関数がasyncじゃないから
-                // あとで分離するので一旦awaitのままで
-
-                // i番目のページへアクセスする(Navigationはひとつでいいのか？sequence毎でいいのか？)
-                const responces: (puppeteer.HTTPResponse | any)[] = await navigation.navigateBy(pageInstances[circulator]!, pageInstances[circulator]!.goto(filterUrl));
-    
-                // filterUrlに対するHTTPResponseを取得する
-                // HTTPResponseからデータを取得する
-                const illustMangaDataElements: iIllustMangaDataElement[] = retrieveDeepProp<iIllustMangaDataElement[]>(["body", "illustManga", "data"], (await responces.shift().json()) as iBodyIncludesIllustManga); 
-                // データからほしい情報をollectedへ格納する
-                collector.resetData(illustMangaDataElements);
-                collectedIds = [...collectedIds, ...collector.execute("id")];
-            });
+            // Reset navigation filter function.
+            .then(() => executor(pageInstances[circulator]!, navigation, keyword, currentPage))
         }
         else {
             console.error("RangeError: Accessing out range of array.");
         }
     }
-
     return sequences;
-
-    // finallyで必ず新規作成したpageインスタンスをcloseすること
-    // となるとclassにしたほうがいいかも
 };
+
+// -- Ver.1 --
+// 
+// const setupTasks = (
+//     browser: puppeteer.Browser,
+//     numberOfProcess: number,    // 作成するpageインスタンスの数
+//     numberOfPages: number,       // 検索結果ページがなんページなのか
+//     ): Promise<void>[] => {
+//     let concurrency: number = numberOfProcess;
+//     const pageInstances: puppeteer.Page[] = [];
+//     const sequences: Promise<void>[] = [];
+//     const navigation: Navigation = new Navigation();
+
+//     for(let i = 0; i < concurrency; i++) {
+//         pageInstances.push(await browser.newPage());
+//         sequences.push(Promise.resolve());
+//     }
+//     // NOTE: iは検索結果ページのページ番号と同じになる
+//     for(let currentPage = 1; currentPage < numberOfPages; currentPage++) {
+//         const circulator: number = currentPage % concurrency;
+//         if(sequences[circulator] !== undefined) {
+//             sequences[circulator] = sequences[circulator]!
+//             .then(() => {
+//                 navigation.resetFilter((res: puppeteer.HTTPResponse) => 
+//                     // TODO: filterUrl = "https:....?p=${i}"となるURLにすること
+//                     res.status() === 200 && res.url() === filterUrl
+//                 );
+                
+//                 // TODO: awaitのエラーはthne()のハンドラ関数がasyncじゃないから
+//                 // あとで分離するので一旦awaitのままで
+
+//                 // i番目のページへアクセスする(Navigationはひとつでいいのか？sequence毎でいいのか？)
+//                 const responces: (puppeteer.HTTPResponse | any)[] = await navigation.navigateBy(pageInstances[circulator]!, pageInstances[circulator]!.goto(filterUrl));
+    
+//                 // filterUrlに対するHTTPResponseを取得する
+//                 // HTTPResponseからデータを取得する
+//                 const illustMangaDataElements: iIllustMangaDataElement[] = retrieveDeepProp<iIllustMangaDataElement[]>(["body", "illustManga", "data"], (await responces.shift().json()) as iBodyIncludesIllustManga); 
+//                 // データからほしい情報をollectedへ格納する
+//                 collector.resetData(illustMangaDataElements);
+//                 collectedIds = [...collectedIds, ...collector.execute("id")];
+//             });
+//         }
+//         else {
+//             console.error("RangeError: Accessing out range of array.");
+//         }
+//     }
+
+//     return sequences;
+
+//     // finallyで必ず新規作成したpageインスタンスをcloseすること
+//     // となるとclassにしたほうがいいかも
+// };
