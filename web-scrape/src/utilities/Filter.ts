f54b0ec9029808e332e593fa0842625d2f63e832
Kabooley <35451559+Kabooley@users.noreply.github.com>
2022-12-01 20:50:07 +0900

    Feat: Add new method in utilities/Filter.ts

--- web-scrape/src/utilities/Filter.ts
+++ web-scrape/src/utilities/Filter.ts
@@ -1,6 +1,47 @@
-/**************************
+/*****************************************************
+ * TEST: 
  * 
+ * **************************************************/ 
+  
+/***
+ * Filters that return only the values that match the necessity keys.
  * 
- * 
- * */
- 
+ * @param {T} target - Target object to be fitlered.
+ * @param {(keyof T)[]} necessity - Property names which going to retrieve from target.
+ * @return {{[Property in keyof T]?: T[Property]}} - Filtered object concist of properties which matched with nessecity.
+ * If no properties are matched, then return empty object.
+ * */   
+const filterOnlyMatchedKey = <T>(
+    target: T,
+    necessity: (keyof T)[]
+    ): {[Property in keyof T]?: T[Property]} => {
+    let filtered = {} as {[Property in keyof T]?: T[Property]};
+    necessity.forEach((key: (keyof T)) => {
+        if(target[key] !== undefined) filtered[key] = target[key];
+            // 上記の方法ではなく下の方法だと次のような謎のオブジェクトが吐き出される 
+            // filtered = {...filtered, ...(target[key] as {[Property in keyof T]?: T[Property]})};
+            // { 0: "E", 1: "d"}
+    });
+    return filtered;
+};
+
+// --- USAGE ---
+// 
+// interface iCollectOptions {
+//     keyword: string;
+//     tags?: string[];
+//     bookmarkOver?: number;
+//     userName?: string;
+// };
+// 
+// const opt: iCollectOptions = {
+//     keyword: "COWBOYBEBOP",
+//     tags: ["cool", "awesome"],
+//     userName: "Ed",
+//     bookmarkOver: 1000
+// };
+// 
+// const validOptions: (keyof iCollectOptions)[] = ["bookmarkOver", "userName"];
+// 
+// console.log(filterOnlyMatchedKey<iCollectOptions>(opt, validOptions));
+  
